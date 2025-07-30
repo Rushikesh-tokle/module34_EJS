34741643b877dfa5b5519d5920cd33d265d90fb6
rushi <rushikesh.tokle@gmail.com>
2025-07-30 12:54:16 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const port =8080;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"")))
+
+app.listen(port,()=>{
+    console.log(`The app is listening at port ${port}`)
+});
+
+   app.get("/",(req,res)=>{
+    res.render("home.ejs")
+   });
+
+ app.get("/rolldice",(req,res)=>{
+    let diceVal=Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs",{num:diceVal})
+ })  
+
+ app.get("/id/:username",(req,res)=>{
+   let {username}=req.params;
+   console.log(username);
+   res.render("instagram",{user:username})
+ })
